da21ac9e9b6322fe00f9960746ccf076d55781a1
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2021-10-10 18:05:02 +0200

    add build folder

---
+++ build/precache-manifest.019b334e700782fec64a90139fff82c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632bfd351bd6439399569b76e618f63e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576b2be5fe46e0a0ab7c",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "7fe2e241244edfdf478d",
+    "url": "/static/css/main.6d2707cf.chunk.css"
+  },
+  {
+    "revision": "576b2be5fe46e0a0ab7c",
+    "url": "/static/js/2.12b9cac3.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.12b9cac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80735016d3c9ba60c09",
+    "url": "/static/js/3.11f2e430.chunk.js"
+  },
+  {
+    "revision": "7fe2e241244edfdf478d",
+    "url": "/static/js/main.7cc54c67.chunk.js"
+  },
+  {
+    "revision": "f3a0376a26f4d2e4485b",
+    "url": "/static/js/runtime-main.d4058ee9.js"
+  },
+  {
+    "revision": "10184dc74146ccf7464fe63f33cc8deb",
+    "url": "/static/media/IMG_4619_crop.10184dc7.jpg"
+  },
+  {
+    "revision": "03c4c6d9e59e5528cdfd7d1aaa69a571",
+    "url": "/static/media/TBL_logo_small_v02.03c4c6d9.svg"
+  }
+]);
